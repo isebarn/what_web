619db73989bb19d2242e42d98fcfc62b2414bbab
david <isebarn182@gmail.com>
2023-03-02 16:42:47 +0000

    add env variables

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://what-api-stage.us.aldryn.io/api/',
+    baseURL: process.env.AXIOS_BASE_URL || 'http://localhost:5000/api/',
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
