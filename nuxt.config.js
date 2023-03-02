7ff28e8fb514debda10ecf8538a91e4d5da14105
david <isebarn182@gmail.com>
2023-03-02 17:58:03 +0000

    Trying to get axios base url working

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,10 +51,16 @@ export default {
     BASE_URL: process.env.BASE_URL
   },  
 
+  publicRuntimeConfig: {
+    axios: {
+      baseURL: process.env.BASE_URL || 'http://localhost:5002/api/'
+    }
+  },
+
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://what-api-stage.us.aldryn.io/api/',
+    baseURL: process.env.BASE_URL || 'http://localhost:5002/api/',
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
