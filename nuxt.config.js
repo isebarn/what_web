71e5a39dc0f887a2be34a1188a253fea92ac02ec
david <isebarn182@gmail.com>
2023-03-02 14:58:33 +0000

    Change baseURL

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:5000/api/',
+    baseURL: 'https://what-api-stage.us.aldryn.io/api/',
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
